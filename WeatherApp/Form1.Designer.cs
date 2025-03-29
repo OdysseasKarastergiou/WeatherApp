f6dbde8f2e7c9d44fd5876020a75d61b8b0169b8
OdysseasKarastergiou <44501721+OdysseasKarastergiou@users.noreply.github.com>
2025-03-29 19:18:06 +0200

    Add files via upload

---
+++ WeatherApp/Form1.Designer.cs
@@ -0,0 +1,261 @@
+﻿namespace WeatherApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.searchTextbox = new System.Windows.Forms.TextBox();
+            this.searchButton = new System.Windows.Forms.Button();
+            this.labelCondition = new System.Windows.Forms.Label();
+            this.labelDetails = new System.Windows.Forms.Label();
+            this.labelSunrise = new System.Windows.Forms.Label();
+            this.labelSunset = new System.Windows.Forms.Label();
+            this.labelWindSpeed = new System.Windows.Forms.Label();
+            this.labelPressure = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.errorLabel = new System.Windows.Forms.Label();
+            this.searchHistoryListBox = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Location = new System.Drawing.Point(69, 47);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(27, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "City:";
+            // 
+            // searchTextbox
+            // 
+            this.searchTextbox.Location = new System.Drawing.Point(102, 44);
+            this.searchTextbox.Name = "searchTextbox";
+            this.searchTextbox.Size = new System.Drawing.Size(100, 20);
+            this.searchTextbox.TabIndex = 1;
+            // 
+            // searchButton
+            // 
+            this.searchButton.Location = new System.Drawing.Point(208, 42);
+            this.searchButton.Name = "searchButton";
+            this.searchButton.Size = new System.Drawing.Size(75, 23);
+            this.searchButton.TabIndex = 2;
+            this.searchButton.Text = "Search";
+            this.searchButton.UseVisualStyleBackColor = true;
+            this.searchButton.Click += new System.EventHandler(this.searchButton_Click);
+            // 
+            // labelCondition
+            // 
+            this.labelCondition.AutoSize = true;
+            this.labelCondition.BackColor = System.Drawing.Color.Transparent;
+            this.labelCondition.Location = new System.Drawing.Point(148, 174);
+            this.labelCondition.Name = "labelCondition";
+            this.labelCondition.Size = new System.Drawing.Size(0, 13);
+            this.labelCondition.TabIndex = 4;
+            // 
+            // labelDetails
+            // 
+            this.labelDetails.AutoSize = true;
+            this.labelDetails.BackColor = System.Drawing.Color.Transparent;
+            this.labelDetails.Location = new System.Drawing.Point(148, 213);
+            this.labelDetails.Name = "labelDetails";
+            this.labelDetails.Size = new System.Drawing.Size(0, 13);
+            this.labelDetails.TabIndex = 5;
+            // 
+            // labelSunrise
+            // 
+            this.labelSunrise.AutoSize = true;
+            this.labelSunrise.BackColor = System.Drawing.Color.Transparent;
+            this.labelSunrise.Location = new System.Drawing.Point(145, 249);
+            this.labelSunrise.Name = "labelSunrise";
+            this.labelSunrise.Size = new System.Drawing.Size(0, 13);
+            this.labelSunrise.TabIndex = 6;
+            // 
+            // labelSunset
+            // 
+            this.labelSunset.AutoSize = true;
+            this.labelSunset.BackColor = System.Drawing.Color.Transparent;
+            this.labelSunset.Location = new System.Drawing.Point(145, 288);
+            this.labelSunset.Name = "labelSunset";
+            this.labelSunset.Size = new System.Drawing.Size(0, 13);
+            this.labelSunset.TabIndex = 7;
+            // 
+            // labelWindSpeed
+            // 
+            this.labelWindSpeed.AutoSize = true;
+            this.labelWindSpeed.BackColor = System.Drawing.Color.Transparent;
+            this.labelWindSpeed.Location = new System.Drawing.Point(515, 174);
+            this.labelWindSpeed.Name = "labelWindSpeed";
+            this.labelWindSpeed.Size = new System.Drawing.Size(0, 13);
+            this.labelWindSpeed.TabIndex = 8;
+            // 
+            // labelPressure
+            // 
+            this.labelPressure.AutoSize = true;
+            this.labelPressure.BackColor = System.Drawing.Color.Transparent;
+            this.labelPressure.Location = new System.Drawing.Point(515, 213);
+            this.labelPressure.Name = "labelPressure";
+            this.labelPressure.Size = new System.Drawing.Size(0, 13);
+            this.labelPressure.TabIndex = 9;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Location = new System.Drawing.Point(69, 174);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(54, 13);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Condition:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Location = new System.Drawing.Point(69, 213);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(42, 13);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Details:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Location = new System.Drawing.Point(69, 249);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(45, 13);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Sunrise:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.Transparent;
+            this.label5.Location = new System.Drawing.Point(69, 288);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(43, 13);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "Sunset:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.Color.Transparent;
+            this.label6.Location = new System.Drawing.Point(304, 174);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(69, 13);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "Wind Speed:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.BackColor = System.Drawing.Color.Transparent;
+            this.label7.Location = new System.Drawing.Point(304, 213);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(51, 13);
+            this.label7.TabIndex = 15;
+            this.label7.Text = "Pressure:";
+            // 
+            // errorLabel
+            // 
+            this.errorLabel.AutoSize = true;
+            this.errorLabel.BackColor = System.Drawing.Color.Transparent;
+            this.errorLabel.Location = new System.Drawing.Point(304, 47);
+            this.errorLabel.Name = "errorLabel";
+            this.errorLabel.Size = new System.Drawing.Size(0, 13);
+            this.errorLabel.TabIndex = 16;
+            // 
+            // searchHistoryListBox
+            // 
+            this.searchHistoryListBox.FormattingEnabled = true;
+            this.searchHistoryListBox.Location = new System.Drawing.Point(560, 42);
+            this.searchHistoryListBox.Name = "searchHistoryListBox";
+            this.searchHistoryListBox.Size = new System.Drawing.Size(228, 368);
+            this.searchHistoryListBox.TabIndex = 17;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.searchHistoryListBox);
+            this.Controls.Add(this.errorLabel);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.labelPressure);
+            this.Controls.Add(this.labelWindSpeed);
+            this.Controls.Add(this.labelSunset);
+            this.Controls.Add(this.labelSunrise);
+            this.Controls.Add(this.labelDetails);
+            this.Controls.Add(this.labelCondition);
+            this.Controls.Add(this.searchButton);
+            this.Controls.Add(this.searchTextbox);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox searchTextbox;
+        private System.Windows.Forms.Button searchButton;
+        private System.Windows.Forms.Label labelCondition;
+        private System.Windows.Forms.Label labelDetails;
+        private System.Windows.Forms.Label labelSunrise;
+        private System.Windows.Forms.Label labelSunset;
+        private System.Windows.Forms.Label labelWindSpeed;
+        private System.Windows.Forms.Label labelPressure;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label errorLabel;
+        private System.Windows.Forms.ListBox searchHistoryListBox;
+    }
+}
+
---
+++ WeatherApp/Form1.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+using System.Net;
+using System.Configuration;
+
+namespace WeatherApp
+{
+    public partial class Form1 : Form
+    {
+        private string APIKey = "05e8f64bd988a5dd9da27e329ee27d3e";
+
+        public Form1()
+        {
+            InitializeComponent();
+            LoadSearchHistory(); // Load history when form opens
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            LoadSearchHistory();
+        }
+
+        private void LoadSearchHistory()
+        {
+            string connectionString = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=" +
+                                      AppDomain.CurrentDomain.BaseDirectory +
+                                      "WeatherApp.mdf;Integrated Security=True;Connect Timeout=30;";
+
+
+            using (var connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                string query = "SELECT CityName, SearchDate FROM SearchHistory ORDER BY SearchDate DESC";
+
+                using (var command = new SqlCommand(query, connection))
+                {
+                    using (var reader = command.ExecuteReader())
+                    {
+                        searchHistoryListBox.Items.Clear(); // Clear previous history
+                        while (reader.Read())
+                        {
+                            string cityName = reader.GetString(0);
+                            DateTime searchDate = reader.GetDateTime(1);
+                            searchHistoryListBox.Items.Add($"{cityName} - {searchDate:g}");
+                        }
+                    }
+                }
+            }
+        }
+
+
+        private void searchButton_Click(object sender, EventArgs e)
+        {
+            GetWeather();
+        }
+
+        private void GetWeather()
+        {
+            try
+            {
+                using (WebClient web = new WebClient())
+                {
+                    string url = $"https://api.openweathermap.org/data/2.5/weather?q={searchTextbox.Text}&appid={APIKey}";
+                    var json = web.DownloadString(url);
+                    WeatherInfo.root Info = JsonConvert.DeserializeObject<WeatherInfo.root>(json);
+
+                    // Display weather details
+                    labelCondition.Text = Info.weather[0].main;
+                    labelDetails.Text = Info.weather[0].description;
+                    labelSunrise.Text = ConvertDateTime(Info.sys.sunrise).ToShortTimeString();
+                    labelSunset.Text = ConvertDateTime(Info.sys.sunset).ToShortTimeString();
+                    labelWindSpeed.Text = Info.wind.speed.ToString();
+                    labelPressure.Text = Info.main.pressure.ToString();
+
+                    // Save search history
+                    SaveSearchHistory(searchTextbox.Text);
+                }
+            }
+            catch (Exception)
+            {
+                // Display an error message if the city is not found
+                labelCondition.Text = "City Doesn't Exist";
+                labelDetails.Text = "";
+                labelSunrise.Text = "";
+                labelSunset.Text = "";
+                labelWindSpeed.Text = "";
+                labelPressure.Text = "";
+            }
+        }
+
+        private void SaveSearchHistory(string cityName)
+        {
+            string connectionString = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=" +
+                                      AppDomain.CurrentDomain.BaseDirectory +
+                                      "WeatherApp.mdf;Integrated Security=True;Connect Timeout=30;";
+
+
+            using (var connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                string query = "INSERT INTO SearchHistory (CityName, SearchDate) VALUES (@CityName, @SearchDate)";
+
+                using (var command = new SqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@CityName", cityName);
+                    command.Parameters.AddWithValue("@SearchDate", DateTime.Now);
+                    command.ExecuteNonQuery();
+                }
+            }
+
+            LoadSearchHistory(); // Reload history after saving
+        }
+
+        private DateTime ConvertDateTime(long sec)
+        {
+            DateTime day = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc).ToLocalTime();
+            return day.AddSeconds(sec).ToLocalTime();
+        }
+    }
+}
